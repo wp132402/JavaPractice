d1f2ee31f62b76084507117cb538ebd3c1ee9006
kms7530 <kms7530@gmail.com>
2015-04-14 15:21:04 +0900

    Car2Test 변경

--- JavaPractice/src/org/dimigo/oop/Car2Test.java
+++ JavaPractice/src/org/dimigo/oop/Car2Test.java
@@ -15,7 +15,7 @@ package org.dimigo.oop;
  * @author	: 곽민석
  * @version	: 1.0
  */
-public class CarTest2 {
+public class Car2Test {
 	public static void main(String[] args) {
 		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
 		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
