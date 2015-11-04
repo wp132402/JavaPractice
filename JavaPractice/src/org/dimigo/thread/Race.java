5dc64c0fa7d94b624140c41d93cab312eb8b0af2
Kwak Minseok <kms7530@gmail.com>
2015-11-04 09:46:59 +0900

    달리기

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * 	|- Race
+ * 
+ * 1. About
+ * 2. Date : 2015. 11. 4.
+ * </pre>
+ * 
+ * @author	: 곽민석
+ * @version	: 1.0
+ */
+public class Race {
+	public static void main(String[] args) {
+		Thread t1 = new Runner("어진");
+		Thread t2 = new Runner("욱재");
+		
+		t1.start();
+		t2.start();
+	}
+}
---
+++ JavaPractice/src/org/dimigo/thread/Runner.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+import java.util.Random;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * 	|- Runner
+ * 
+ * 1. About
+ * 2. Date : 2015. 11. 4.
+ * </pre>
+ * 
+ * @author	: 곽민석
+ * @version	: 1.0
+ */
+public class Runner extends Thread {
+	private String name;
+	
+	public Runner() {
+		
+	}
+	
+	public Runner(String name) {
+		this.name = name;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public void run() {
+		for(int i = 100;i >= 0;i-=10) {
+			int time = new Random().nextInt(1000);
+			
+			System.out.println(name + " " + i + "미터");
+			try {
+				Thread.sleep(time);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(name + " 도착");
+	}
+}
